aebefab77ed2eb818c4cba0dd7fde6f6f1a359af
Mike McQuaid <mike@mikemcquaid.com>
2016-05-28 21:40:02 +0100

    utils/curl: support reading curl's output.

--- Library/Homebrew/utils/curl.rb
+++ Library/Homebrew/utils/curl.rb
@@ -1,16 +1,24 @@
 require "pathname"
 
-def curl(*args)
+def curl_args(extra_args=[])
   curl = Pathname.new ENV["HOMEBREW_CURL"]
   curl = Pathname.new "/usr/bin/curl" unless curl.exist?
   raise "#{curl} is not executable" unless curl.exist? && curl.executable?
 
   flags = HOMEBREW_CURL_ARGS
-  flags = flags.delete("#") if ARGV.verbose?
+  flags -= ["--progress-bar"] if ARGV.verbose?
 
-  args = [flags, HOMEBREW_USER_AGENT_CURL, *args]
+  args = ["#{curl}"] + flags + extra_args
   args << "--verbose" if ENV["HOMEBREW_CURL_VERBOSE"]
   args << "--silent" if !$stdout.tty? || ENV["TRAVIS"]
+  args
+end
 
-  safe_system curl, *args
+def curl(*args)
+  safe_system(*curl_args(args))
+end
+
+def curl_output(*args)
+  curl_args = curl_args(args) - ["--fail"]
+  Utils.popen_read_text(*curl_args)
 end
