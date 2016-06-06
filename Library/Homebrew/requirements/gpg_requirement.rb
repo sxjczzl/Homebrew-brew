00cf131ac8d8671315143341fac0f4759e961d0e
Dominyk Tiller <dominyktiller@gmail.com>
2016-06-06 03:55:16 +0100

    gpg_requirement: add stronger detection methods
    It's unsafe to assume every gpg executable is going to forever equal 1.x and
    every gpg2 executable is going to forever equal 2.x. MacGPG2 has been symlinking
    2.x as a vanilla `gpg` for a while, for example.
    
    This adds stronger detection methods to determine regardless of what the executable
    is called which version it actually represents.

--- Library/Homebrew/requirements/gpg_requirement.rb
+++ Library/Homebrew/requirements/gpg_requirement.rb
@@ -4,5 +4,26 @@ class GPGRequirement < Requirement
   fatal true
   default_formula "gnupg2"
 
-  satisfy { which("gpg2") || which("gpg") }
+  satisfy(:build_env => false) { gpg2 || gpg1 }
+
+  # Homebrew installs GnuPG 1.x as `gpg` but this may change in future.
+  # MacGPG2/GPGTools installs GnuPG 2.0.x as a vanilla `gpg` symlink
+  # pointing to `gpg2`. Ensure we're not using unexpected gpg version.
+  def gpg1
+    which_all("gpg").detect do |gpg|
+      gpg_version = Utils.popen_read(gpg, "--version")[/\d\.\d/, 0]
+      next unless gpg_version
+      Version.new(gpg_version.to_s) == Version.new("1.4")
+    end
+  end
+
+  def gpg2
+    which_all("gpg2").detect do |gpg2|
+      gpg2_version = Utils.popen_read(gpg2, "--version")[/\d\.\d/, 0]
+      next unless gpg2_version
+      # For now, to give upstreams time to adjust, only support 2.0.x
+      # rather than the 2.1.x "modern" series.
+      Version.new(gpg2_version.to_s) == Version.new("2.0")
+    end
+  end
 end
