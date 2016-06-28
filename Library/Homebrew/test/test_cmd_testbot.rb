a719a0ae7e213380291e5f43aaaa65939ff369ba
Tim D. Smith <git@tim-smith.us>
2016-06-28 11:35:52 -0700

    address Martin's feedback

--- Library/Homebrew/test/test_cmd_testbot.rb
+++ Library/Homebrew/test/test_cmd_testbot.rb
@@ -2,43 +2,44 @@ require "testing_env"
 require "dev-cmd/test-bot"
 
 class TestbotCommandTests < Homebrew::TestCase
-  def with_environment(h)
-    old = Hash[h.keys.map { |k| [k, ENV[k]] }]
-    ENV.update h
+  def with_environment(partial_env)
+    old = ENV.to_hash
+    ENV.update partial_env
     begin
       yield
     ensure
-      ENV.update old
+      ENV.replace old
     end
   end
 
   def test_resolve_test_tap
-    tap = Homebrew::resolve_test_tap
-    assert_equal tap, nil, "Should return nil if no tap slug provided"
+    tap = Homebrew.resolve_test_tap
+    assert_nil tap, "Should return nil if no tap slug provided"
 
     slug = "spam/homebrew-eggs"
     url = "https://github.com/#{slug}.git"
-    pairs = [
+    environments = [
       {"TRAVIS_REPO_SLUG" => slug},
       {"UPSTREAM_BOT_PARAMS" => "--tap=#{slug}"},
+      {"UPSTREAM_BOT_PARAMS" => "--tap=spam/eggs"},
       {"UPSTREAM_GIT_URL" => url},
       {"GIT_URL" => url},
     ]
 
-    predicate = Proc.new do
-      tap = Homebrew::resolve_test_tap
-      assert_kind_of Tap, tap
-      assert_equal tap.user, "spam"
-      assert_equal tap.repo, "eggs"
+    predicate = Proc.new do |message|
+      tap = Homebrew.resolve_test_tap
+      assert_kind_of Tap, tap, message
+      assert_equal tap.user, "spam", message
+      assert_equal tap.repo, "eggs", message
     end
 
-    pairs.each do |pair|
+    environments.each do |pair|
       with_environment(pair) do
-        predicate.call
+        predicate[pair.to_s]
       end
     end
 
     ARGV.expects(:value).with("tap").returns(slug)
-    predicate.call
+    predicate["ARGV"]
   end
 end
