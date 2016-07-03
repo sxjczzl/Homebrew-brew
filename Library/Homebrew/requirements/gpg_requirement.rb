e55c7d2dd0e12f743d5a726ee960a21f8a923765
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-03 04:21:06 +0100

    gpg_requirement: version => short_version for clarity

--- Library/Homebrew/requirements/gpg_requirement.rb
+++ Library/Homebrew/requirements/gpg_requirement.rb
@@ -11,19 +11,19 @@ class GPGRequirement < Requirement
   # pointing to `gpg2`. Ensure we're not using unexpected gpg version.
   def gpg1
     which_all("gpg").detect do |gpg|
-      gpg_version = Utils.popen_read(gpg, "--version")[/\d\.\d/, 0]
-      next unless gpg_version
-      Version.new(gpg_version.to_s) == Version.new("1.4")
+      gpg_short_version = Utils.popen_read(gpg, "--version")[/\d\.\d/, 0]
+      next unless gpg_short_version
+      Version.new(gpg_short_version.to_s) == Version.new("1.4")
     end
   end
 
   def gpg2
     which_all("gpg2").detect do |gpg2|
-      gpg2_version = Utils.popen_read(gpg2, "--version")[/\d\.\d/, 0]
-      next unless gpg2_version
+      gpg2_short_version = Utils.popen_read(gpg2, "--version")[/\d\.\d/, 0]
+      next unless gpg2_short_version
       # For now, to give upstreams time to adjust, only support 2.0.x
       # rather than the 2.1.x "modern" series.
-      Version.new(gpg2_version.to_s) == Version.new("2.0")
+      Version.new(gpg2_short_version.to_s) == Version.new("2.0")
     end
   end
 end
