e449d7ffa9be8f8e1a26cdc9efacaacde882546f
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-18 02:13:12 +0100

    gpg: add create_test_key method

--- Library/Homebrew/gpg.rb
+++ Library/Homebrew/gpg.rb
@@ -6,4 +6,20 @@ class Gpg
   def self.available?
     File.exist?(GPG_EXECUTABLE).to_s && File.executable?(GPG_EXECUTABLE)
   end
+
+  def self.create_test_key(path)
+    raise "No GPG present to test against!" unless available?
+
+    (path/"batchgpg").write <<-EOS.undent
+      Key-Type: RSA
+      Key-Length: 2048
+      Subkey-Type: RSA
+      Subkey-Length: 2048
+      Name-Real: Testing
+      Name-Email: testing@foo.bar
+      Expire-Date: 1d
+      %commit
+    EOS
+    system GPG_EXECUTABLE, "--batch", "--gen-key", "batchgpg"
+  end
 end
