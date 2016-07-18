fd0ed6a0fb5a82e1ea0c9212a5b84a8fd370e06a
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-18 01:39:46 +0100

    gpg_requirement: drop gpg1 support
    We're going to leap over Debian in renaming GPG 1.x `gpg1`. We're not going
    to do it completely, you'll still be able to plonk the `libexec/bin` path of `gpg`
    in your PATH to access a vanilla `gpg` 1.x executable if you want to, but we're not
    going to actively keep adding gpg1 support to formulae going forwards.
    
    There's really no reason why 99.9% of projects should not or cannot use `gpg2`
    these days, and unlike a later transition to GPG 2.1.x the transition to `gpg2`
    should be pretty smooth. Yes, famous last words I'm absolutely sure.

--- Library/Homebrew/requirements/gpg_requirement.rb
+++ Library/Homebrew/requirements/gpg_requirement.rb
@@ -4,16 +4,17 @@ class GPGRequirement < Requirement
   fatal true
   default_formula "gnupg2"
 
-  satisfy(:build_env => false) { gpg2 || gpg1 }
+  satisfy(:build_env => false) { gpg2 || gpg }
 
-  # Homebrew installs GnuPG 1.x as `gpg` but this may change in future.
   # MacGPG2/GPGTools installs GnuPG 2.0.x as a vanilla `gpg` symlink
-  # pointing to `gpg2`. Ensure we're not using unexpected gpg version.
-  def gpg1
+  # pointing to `gpg2`. Ensure we're using the expected 2.0 `gpg`.
+  # For now, to give upstreams time to adjust, only support 2.0.x
+  # rather than the 2.1.x "modern" series.
+  def gpg
     which_all("gpg").detect do |gpg|
       gpg_short_version = Utils.popen_read(gpg, "--version")[/\d\.\d/, 0]
       next unless gpg_short_version
-      Version.new(gpg_short_version.to_s) == Version.new("1.4")
+      Version.new(gpg_short_version.to_s) == Version.new("2.0")
     end
   end
 
@@ -21,8 +22,6 @@ class GPGRequirement < Requirement
     which_all("gpg2").detect do |gpg2|
       gpg2_short_version = Utils.popen_read(gpg2, "--version")[/\d\.\d/, 0]
       next unless gpg2_short_version
-      # For now, to give upstreams time to adjust, only support 2.0.x
-      # rather than the 2.1.x "modern" series.
       Version.new(gpg2_short_version.to_s) == Version.new("2.0")
     end
   end
