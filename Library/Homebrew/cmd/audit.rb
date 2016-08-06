6f09e8287291cf7224d077b7c30af488e1d344d0
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-06 15:25:57 +0100

    audit: tweak include wording

--- Library/Homebrew/cmd/audit.rb
+++ Library/Homebrew/cmd/audit.rb
@@ -203,7 +203,7 @@ class FormulaAuditor
     return unless @strict
 
     component_list = [
-      [/^  include Language::/,            "include method"],
+      [/^  include Language::/,            "include directive"],
       [/^  desc ["'][\S\ ]+["']/,          "desc"],
       [/^  homepage ["'][\S\ ]+["']/,      "homepage"],
       [/^  url ["'][\S\ ]+["']/,           "url"],
