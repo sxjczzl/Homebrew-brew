784d964b6d1b269244602c86837ac604dde9117e
Tim D. Smith <git@tim-smith.us>
2016-08-11 07:42:19 -0700

    Fix tests

--- Library/Homebrew/test/test_sandbox.rb
+++ Library/Homebrew/test/test_sandbox.rb
@@ -28,18 +28,27 @@ class SandboxTest < Homebrew::TestCase
 
   def test_complains_on_failure
     Utils.expects(:popen_read => "foo")
+    ARGV.stubs(:verbose? => true)
     out, err = capture_io do
       assert_raises(ErrorDuringExecution) { @sandbox.exec "false" }
     end
-    assert_match out, "foo"
+    assert_match "foo", out
   end
 
   def test_ignores_bogus_python_error
-    bogus_error = "Mar 17 02:55:06 sandboxd[342]: Python(49765) deny file-write-unlink /System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/distutils/errors.pyc\n"
-    Utils.expects(:popen_read => bogus_error)
+    with_bogus_error = <<-EOS.undent
+      foo
+      Mar 17 02:55:06 sandboxd[342]: Python(49765) deny file-write-unlink /System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/distutils/errors.pyc
+      bar
+    EOS
+    Utils.expects(:popen_read => with_bogus_error)
+    ARGV.stubs(:verbose? => true)
     out, err = capture_io do
       assert_raises(ErrorDuringExecution) { @sandbox.exec "false" }
     end
-    assert_predicate out, :empty?
+    refute_predicate out, :empty?
+    assert_match "foo", out
+    assert_match "bar", out
+    refute_match "Python", out
   end
 end
