821f8ead91ebb2d1107c06018d139477a94fae75
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-22 15:46:16 +0100

    test_gpg: add syntax valid test

--- Library/Homebrew/test/test_gpg.rb
+++ Library/Homebrew/test/test_gpg.rb
@@ -7,6 +7,10 @@ class GpgTest < Homebrew::TestCase
     @dir = Pathname.new(mktmpdir)
   end
 
+  def teardown
+    FileUtils.rm_rf(@dir)
+  end
+
   def test_create_test_key
     Dir.chdir(@dir) do
       with_environment("HOME" => @dir) do
@@ -14,7 +18,13 @@ class GpgTest < Homebrew::TestCase
         assert_predicate @dir/".gnupg/secring.gpg", :exist?
       end
     end
-  ensure
-    @dir.rmtree
+  end
+
+  def test_formula_syntax_valid
+    f = formula do
+      url "https://ftpmirror.gnu.org/wget/wget-1.18.tar.xz"
+      gpg "https://ftpmirror.gnu.org/wget/wget-1.18.tar.xz.sig"
+    end
+    assert_equal "https://ftpmirror.gnu.org/wget/wget-1.18.tar.xz.sig", f.gpg
   end
 end
