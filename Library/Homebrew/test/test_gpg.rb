b02577fbb6c9a8f91abe7e6e0d0dd6157d628006
Dominyk Tiller <dominyktiller@gmail.com>
2016-08-23 03:03:07 +0100

    test_gpg: add test_formula_resource_syntax_valid

--- Library/Homebrew/test/test_gpg.rb
+++ Library/Homebrew/test/test_gpg.rb
@@ -27,4 +27,16 @@ class GpgTest < Homebrew::TestCase
     end
     assert_equal "https://ftpmirror.gnu.org/wget/wget-1.18.tar.xz.sig", f.gpg
   end
+
+  def test_formula_resource_syntax_valid
+    f = formula do
+      url "https://ftpmirror.gnu.org/wget/wget-1.18.tar.xz"
+
+      resource("doom") do
+        url "https://test.doom/doomed-1.2.3.tar.gz"
+        gpg "https://test.doom/doomed-1.2.3.tar.gz.sig"
+      end
+    end
+    assert_equal "https://test.doom/doomed-1.2.3.tar.gz.sig", f.resource("doom").gpg
+  end
 end
