0db155f5018c39a2cb8cac3fae8dc49fb10b9eed
J Huang <i@jhuang.me>
2016-09-15 14:40:54 -0300

    os_mac_os_name: add test with version

--- Library/Homebrew/test/test_os_mac_os_name.rb
+++ Library/Homebrew/test/test_os_mac_os_name.rb
@@ -8,4 +8,11 @@ class OSMacOsNameTests < Homebrew::TestCase
     assert_equal "macOS", OS::Mac.os_name("10.12")
     assert_equal "macOS", OS::Mac.os_name("10.13")
   end
+
+  def test_os_name_with_version
+    assert_equal "OS X", OS::Mac.os_name(Version.create("10.9"))
+    assert_equal "OS X", OS::Mac.os_name(Version.create("10.11"))
+    assert_equal "macOS", OS::Mac.os_name(Version.create("10.12"))
+    assert_equal "macOS", OS::Mac.os_name(Version.create("10.13"))
+  end
 end
