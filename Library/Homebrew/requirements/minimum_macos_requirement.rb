ba147c10b6aad509ace8ed8086ec4ac3375c9250
J Huang <i@jhuang.me>
2016-09-14 21:56:29 -0300

    minimum_macos_requirement: use os_name

--- Library/Homebrew/requirements/minimum_macos_requirement.rb
+++ Library/Homebrew/requirements/minimum_macos_requirement.rb
@@ -11,6 +11,6 @@ class MinimumMacOSRequirement < Requirement
   satisfy(:build_env => false) { MacOS.version >= @version }
 
   def message
-    "OS X #{@version.pretty_name} or newer is required."
+    "#{MacOS.os_name(@version)} #{@version.pretty_name} or newer is required."
   end
 end
