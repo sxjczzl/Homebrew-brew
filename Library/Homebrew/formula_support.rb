240ba5b062bd3bb22e48c41ec367b1abc6afd58c
J Huang <i@jhuang.me>
2016-09-14 22:42:58 -0300

    formula_support: replace OS X in comment to macOS

--- Library/Homebrew/formula_support.rb
+++ Library/Homebrew/formula_support.rb
@@ -1,7 +1,7 @@
 # Used to track formulae that cannot be installed at the same time
 FormulaConflict = Struct.new(:name, :reason)
 
-# Used to annotate formulae that duplicate OS X provided software
+# Used to annotate formulae that duplicate macOS provided software
 # or cause conflicts when linked in.
 class KegOnlyReason
   def initialize(reason, explanation)
