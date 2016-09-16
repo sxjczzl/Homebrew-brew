fb255d82d0492066290d0bdde633d7d3064a9aa7
J Huang <i@jhuang.me>
2016-09-16 11:34:58 -0300

    add a dummy KegOnlyReason for non-macOS platform
    Ideally on non-macOS platform we should do nothing on keg_only except we expand its concept

--- Library/Homebrew/formula_support.rb
+++ Library/Homebrew/formula_support.rb
@@ -1,6 +1,23 @@
 # Used to track formulae that cannot be installed at the same time
 FormulaConflict = Struct.new(:name, :reason)
 
+# A dummy KegOnlyReason class that adds `keg_only` compatibility across non-macOS platforms
+class KegOnlyReason
+  def initialize(reason, explanation)
+    @reason = reason
+    @explanation = explanation
+  end
+
+  def valid?
+    true
+  end
+
+  def to_s
+    return @explanation unless @explanation.empty?
+    @reason
+  end
+end
+
 # Used to annotate formulae that don't require compiling or cannot build bottle.
 class BottleDisableReason
   SUPPORTED_TYPES = [:unneeded, :disable]
