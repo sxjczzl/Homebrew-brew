9610ea16e4736962374d7035889dcb23f43aebbd
Shaun Jackman <sjackman@gmail.com>
2016-11-28 10:17:40 -0800

    Tap.formula_files: Sort files to be determinstic
    Pathname.children returns files in an arbitrary order that
    depends on the file system, making this function both
    non-determinstic and variable depending the configuration of
    the host system.

--- Library/Homebrew/tap.rb
+++ Library/Homebrew/tap.rb
@@ -303,7 +303,7 @@ class Tap
   # an array of all {Formula} files of this {Tap}.
   def formula_files
     @formula_files ||= if formula_dir
-      formula_dir.children.select(&method(:formula_file?))
+      formula_dir.children.select(&method(:formula_file?)).sort
     else
       []
     end
@@ -312,7 +312,7 @@ class Tap
   # an array of all {Cask} files of this {Tap}.
   def cask_files
     @cask_files ||= if cask_dir
-      cask_dir.children.select(&method(:cask_file?))
+      cask_dir.children.select(&method(:cask_file?)).sort
     else
       []
     end
