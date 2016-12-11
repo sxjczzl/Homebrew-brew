c9c46c6d9331454ed4d7bc463a30ff9744f9f4d4
Gregory Nisbet <gregory.nisbet@gmail.com>
2016-12-11 15:57:42 -0800

    Style

--- Library/Homebrew/test/virtualenv_test.rb
+++ Library/Homebrew/test/virtualenv_test.rb
@@ -5,7 +5,7 @@ class VirtualenvTest < Homebrew::TestCase
     brew = HOMEBREW_LIBRARY_PATH.parent.parent/"bin/brew"
     virtualenv_message = "Cowardly refusing to run inside virtualenv, please deactivate\n"
     output = Utils.popen_read("env VIRTUAL_ENV=1 /bin/bash #{brew} 2>&1")
-    assert (not $?.success?)
+    assert !$?.success?
     assert_equal output, virtualenv_message
   end
 end
