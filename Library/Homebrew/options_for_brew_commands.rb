6dbc797ed3c4c47457639b4ae4b610958f2cb2e6
Raza Hussain <razahussain@Razas-MacBook-Pro.local>
2017-04-10 11:53:35 -0400

    print options and their desciptions

--- Library/Homebrew/options_for_brew_commands.rb
+++ Library/Homebrew/options_for_brew_commands.rb
@@ -11,13 +11,16 @@ module Homebrew
     def check_invalid_options
       invalid_options_by_user = []
       ARGV.options_only.each do |option|
-        invalid_options_by_user << option unless @valid_options.include?(option)
+        invalid_options_by_user << option unless @valid_options.key?(option)
       end
       invalid_options_by_user = invalid_options_by_user.uniq
       return if invalid_options_by_user.empty?
       odie <<-EOS.undent
         #{Formatter.pluralize(invalid_options_by_user.length, "Invalid Option")} Provided: #{invalid_options_by_user.join " "}
         #{"Only #{Formatter.pluralize(@valid_options.length, "Option")} Valid: #{@valid_options.keys.join " "}" unless @valid_options.empty?}
+
+            #{@valid_options.map { |k, v| "#{k}:  #{v}" }.join("\n    ")}
+
       EOS
     end
   end
