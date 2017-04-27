760553c71f1719b78d7f77079f93bdd98f52f99a
Raza Hussain <razahussain@Razas-MacBook-Pro.local>
2017-04-27 01:33:15 -0400

    making generic the tests that invoke brew

--- Library/Homebrew/test/command_options_spec.rb
+++ Library/Homebrew/test/command_options_spec.rb
@@ -22,6 +22,15 @@ def get_valid_options(cmd)
   return valid_options
 end
 
+def random_invalid_options(valid_options, array_length)
+  invalid_options = []
+  while true do
+    rand = ("a".."z").to_a.sample(5).join
+    invalid_options.push(rand) if valid_options.include?(rand) == false && invalid_options.include?(rand) == false
+    return invalid_options if invalid_options.length == array_length
+  end
+end
+
 describe Homebrew::CommandOptions do
   describe "brew commands", :integration_test do
     cmd = "commands"
@@ -46,76 +55,34 @@ describe Homebrew::CommandOptions do
     end
 
     it "test # 1: runs correctly if no option is provided" do
-      expect { brew "commands" }
-        .to output(/Built-in commands/).to_stdout
+      expect { brew cmd }
+        .to output.to_stdout
         .and be_a_success
     end
 
     valid_options = get_valid_options(cmd)
     valid_options.each do |option, _desc|
       it "test # 2: runs correctly if any 1 correct option is provided" do
-        expect { system("brew commands #{option}") }
-          .to output(/--cache/).to_stdout_from_any_process
+        expect { system("brew #{cmd} #{option}") }
+          .to output.to_stdout_from_any_process
       end
     end
 
     it "test # 3: runs correctly if all correct options are provided" do
-      expect { system("brew commands #{valid_options.keys.join " "}") }
-        .to output(/--cache/).to_stdout_from_any_process
+      expect { system("brew #{cmd} #{valid_options.keys.join " "}") }
+        .to output.to_stdout_from_any_process
     end
 
     it "test # 4: returns error if 1 incorrect option is provided" do
-      expect { system("brew commands --quietee") }
-        .to output(<<-MESSAGE.undent).to_stderr_from_any_process
-		  Error: 1 invalid option provided: --quietee
-		  <#{cmd}> has only 2 valid options: --quiet --include-aliases
-
-		      --quiet:  List only the names of commands without the header
-		      --include-aliases:  The aliases of internal commands will be included
-
-		MESSAGE
+      invalid_option = "--#{random_invalid_options(valid_options, 1)[0]}"
+      expect { system("brew #{cmd} #{invalid_option}") }
+        .to output(/invalid option provided/).to_stderr_from_any_process
     end
 
     it "test # 5: returns error if more than 1 incorrect options are provided" do
-      expect { system("brew commands --quietee --qute --foo1 --foo2") }
-        .to output(<<-MESSAGE.undent).to_stderr_from_any_process
-		  Error: 4 invalid options provided: --quietee --qute --foo1 --foo2
-		  <#{cmd}> has only 2 valid options: --quiet --include-aliases
-
-		      --quiet:  List only the names of commands without the header
-		      --include-aliases:  The aliases of internal commands will be included
-
-		MESSAGE
-    end
-
-    # The following Test # 6 can be used for other cmds as well
-    all_argv_options = [["--foo"],
-                        ["--bar", "--bar1", "--bar2"],
-                        ["--quietee", "--qute", "--foo1", "--foo2", "--quiet"]]
-    all_argv_options.each do |argv_options|
-      it "test # 6: returns error if incorrect option(s) provided" do
-        invalid_options_by_user = (argv_options - valid_options.keys).uniq
-        invalid_option_pluralize = Formatter.pluralize(invalid_options_by_user.length, "invalid option")
-        valid_option_pluralize = Formatter.pluralize(valid_options.length, "valid option")
-        invalid_option_string = "#{invalid_option_pluralize} provided: #{invalid_options_by_user.join " "}"
-
-        expect = expect { system("brew commands #{argv_options.join " "}") }
-        if valid_options.empty?
-          expect.to output(<<-MESSAGE.undent).to_stderr_from_any_process
-            Error: #{invalid_option_string}
-            <#{cmd}> has no valid options
-
-          MESSAGE
-        else
-          expect.to output(<<-MESSAGE.undent).to_stderr_from_any_process
-            Error: #{invalid_option_string}
-            <#{cmd}> has only #{valid_option_pluralize}: #{valid_options.keys.join " "}
-
-                #{valid_options.map { |k, v| "#{k}:  #{v}" }.join("\n    ")}
-
-          MESSAGE
-        end
-      end
+      invalid_options = random_invalid_options(valid_options, 3)
+      expect { system("brew #{cmd} --#{invalid_options.join " --"}") }
+        .to output(/invalid options provided/).to_stderr_from_any_process
     end
   end
 end
