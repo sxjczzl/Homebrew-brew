19080db1a21aa86e54e90347f43ceffb73c557ef
Raza Hussain <razahussain@Razas-MacBook-Pro.local>
2017-05-07 07:57:25 -0400

    one class per command approach implemented

--- Library/Homebrew/cmd/commands.rb
+++ Library/Homebrew/cmd/commands.rb
@@ -6,58 +6,59 @@
 
 module Homebrew
   module_function
-
-  def commands
-    options do
+  class InstallCommand < CommandOptions
+    Homebrew.options do
       option "--quiet", "List only the names of commands without the header"
       option "--include-aliases", "The aliases of internal commands will be included"
     end
+    
+    def self.commands
+      if ARGV.include? "--quiet"
+        cmds = internal_commands + external_commands
+        cmds += internal_developer_commands
+        cmds += HOMEBREW_INTERNAL_COMMAND_ALIASES.keys if ARGV.include? "--include-aliases"
+        puts Formatter.columns(cmds.sort)
+      else
+        # Find commands in Homebrew/cmd
+        puts "Built-in commands"
+        puts Formatter.columns(internal_commands)
 
-    if ARGV.include? "--quiet"
-      cmds = internal_commands + external_commands
-      cmds += internal_developer_commands
-      cmds += HOMEBREW_INTERNAL_COMMAND_ALIASES.keys if ARGV.include? "--include-aliases"
-      puts Formatter.columns(cmds.sort)
-    else
-      # Find commands in Homebrew/cmd
-      puts "Built-in commands"
-      puts Formatter.columns(internal_commands)
-
-      # Find commands in Homebrew/dev-cmd
-      puts
-      puts "Built-in developer commands"
-      puts Formatter.columns(internal_developer_commands)
-
-      # Find commands in the path
-      unless (exts = external_commands).empty?
+        # Find commands in Homebrew/dev-cmd
         puts
-        puts "External commands"
-        puts Formatter.columns(exts)
+        puts "Built-in developer commands"
+        puts Formatter.columns(internal_developer_commands)
+
+        # Find commands in the path
+        unless (exts = external_commands).empty?
+          puts
+          puts "External commands"
+          puts Formatter.columns(exts)
+        end
       end
     end
-  end
 
-  def internal_commands
-    find_internal_commands HOMEBREW_LIBRARY_PATH/"cmd"
-  end
+    def self.internal_commands
+      find_internal_commands HOMEBREW_LIBRARY_PATH/"cmd"
+    end
 
-  def internal_developer_commands
-    find_internal_commands HOMEBREW_LIBRARY_PATH/"dev-cmd"
-  end
+    def self.internal_developer_commands
+      find_internal_commands HOMEBREW_LIBRARY_PATH/"dev-cmd"
+    end
 
-  def external_commands
-    paths.each_with_object([]) do |path, cmds|
-      Dir["#{path}/brew-*"].each do |file|
-        next unless File.executable?(file)
-        cmd = File.basename(file, ".rb")[5..-1]
-        cmds << cmd unless cmd.include?(".")
+    def self.external_commands
+      paths.each_with_object([]) do |path, cmds|
+        Dir["#{path}/brew-*"].each do |file|
+          next unless File.executable?(file)
+          cmd = File.basename(file, ".rb")[5..-1]
+          cmds << cmd unless cmd.include?(".")
+        end
+      end.sort
+    end
+
+    def self.find_internal_commands(directory)
+      directory.children.each_with_object([]) do |f, cmds|
+        cmds << f.basename.to_s.sub(/\.(?:rb|sh)$/, "") if f.file?
       end
-    end.sort
-  end
-
-  def find_internal_commands(directory)
-    directory.children.each_with_object([]) do |f, cmds|
-      cmds << f.basename.to_s.sub(/\.(?:rb|sh)$/, "") if f.file?
     end
   end
 end
