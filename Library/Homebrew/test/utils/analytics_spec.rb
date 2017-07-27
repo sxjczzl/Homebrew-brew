782776434b7461950e4e59aaaf2368b30c263c29
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-07-27 17:47:28 +0530

    Added tests for report and report_event

--- Library/Homebrew/test/utils/analytics_spec.rb
+++ Library/Homebrew/test/utils/analytics_spec.rb
@@ -8,8 +8,12 @@ describe Utils::Analytics do
       end
 
       it "includes CI when ENV['CI'] is set" do
-        allow(ENV).to receive(:[]).with("CI").and_return("true")
+        CI = ENV["CI"]
+        ENV["CI"] = "true"
+
         expect(described_class.os_prefix_ci).to include("CI")
+
+        ENV["CI"] = CI
       end
 
       it "does not include prefix when HOMEBREW_PREFIX is usr/local" do
@@ -28,7 +32,62 @@ describe Utils::Analytics do
     end
   end
 
-  describe "::" do
-    
+  describe "::report_event" do
+    before(:all) do
+      ANALYTICS_DEBUG = ENV["HOMEBREW_ANALYTICS_DEBUG"]
+      NO_ANALYTICS_THIS_RUN = ENV["HOMEBREW_NO_ANALYTICS_THIS_RUN"]
+      NO_ANALYTICS = ENV["HOMEBREW_NO_ANALYTICS"]
+    end
+
+    let(:f) { formula { url "foo-1.0" } }
+    let(:options) { FormulaInstaller.new(f).display_options(f) }
+    let(:action)  { "#{f.full_name} #{options}".strip }
+
+    context "when ENV vars is set" do
+      it "returns nil when HOMEBREW_NO_ANALYTICS is true" do
+        ENV["HOMEBREW_NO_ANALYTICS"] = "true"
+
+        expect(described_class.report_event("install", action)).to be_nil
+
+        ENV["HOMEBREW_NO_ANALYTICS"] = NO_ANALYTICS
+      end
+
+      it "returns nil when HOMEBREW_NO_ANALYTICS_THIS_RUN is true" do
+        ENV["HOMEBREW_NO_ANALYTICS_THIS_RUN"] = "true"
+
+        expect(described_class.report_event("install", action)).to be_nil
+
+        ENV["HOMEBREW_NO_ANALYTICS_THIS_RUN"] = NO_ANALYTICS_THIS_RUN
+      end
+
+      # [WIP]
+      it "returns nil when HOMEBREW_ANALYTICS_DEBUG is true" do
+        ENV["HOMEBREW_ANALYTICS_DEBUG"] = "true"
+        ENV["HOMEBREW_NO_ANALYTICS"] = nil
+        ENV["HOMEBREW_NO_ANALYTICS_THIS_RUN"] = nil
+
+        shutup do
+          described_class.report_event("install", action)
+        end
+
+        ENV["HOMEBREW_ANALYTICS_DEBUG"] = ANALYTICS_DEBUG
+        ENV["HOMEBREW_NO_ANALYTICS"] = NO_ANALYTICS
+        ENV["HOMEBREW_NO_ANALYTICS_THIS_RUN"] = NO_ANALYTICS_THIS_RUN
+      end
+    end
+
+    context "when ENV vars are nil" do
+      it "returns nil when HOMEBREW_ANALYTICS_DEBUG is not set" do
+        ENV["HOMEBREW_NO_ANALYTICS"] = nil
+        ENV["HOMEBREW_NO_ANALYTICS_THIS_RUN"] = nil
+
+        shutup do
+          expect(described_class.report_event("install", action)).to be_an_instance_of(Thread)
+        end
+
+        ENV["HOMEBREW_NO_ANALYTICS"] = NO_ANALYTICS
+        ENV["HOMEBREW_NO_ANALYTICS_THIS_RUN"] = NO_ANALYTICS_THIS_RUN
+      end
+    end
   end
 end
