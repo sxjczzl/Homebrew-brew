89972b84e3b44bf695de3ca70f022af3428e50ac
Maxim Belkin <maxim.belkin@gmail.com>
2017-09-19 05:11:10 -0500

    test.rb: test all installed formulae with --installed flag

--- Library/Homebrew/dev-cmd/test.rb
+++ Library/Homebrew/dev-cmd/test.rb
@@ -1,4 +1,4 @@
-#:  * `test` [`--devel`|`--HEAD`] [`--debug`] [`--keep-tmp`] <formula>:
+#:  * `test` [`--devel`|`--HEAD`] [`--debug`] [`--keep-tmp`] [--installed|<formula>]:
 #:    Most formulae provide a test method. `brew test` <formula> runs this
 #:    test method. There is no standard output or return code, but it should
 #:    generally indicate to the user if something is wrong with the installed
@@ -13,6 +13,8 @@
 #:    If `--keep-tmp` is passed, the temporary files created for the test are
 #:    not deleted.
 #:
+#:    If `--installed` is passed, test all installed formulae
+#:
 #:    Example: `brew install jruby && brew test jruby`
 
 require "extend/ENV"
@@ -24,9 +26,17 @@ module Homebrew
   module_function
 
   def test
-    raise FormulaUnspecifiedError if ARGV.named.empty?
+    raise FormulaUnspecifiedError if ARGV.named.empty? && !ARGV.include?("--installed")
+    raise "Specifying formulae together with '--installed' flag is not supported " if !ARGV.named.empty? && ARGV.include?("--installed")
 
-    ARGV.resolved_formulae.each do |f|
+    if ARGV.named.empty?
+      enum = Formula.installed.each
+    else
+      enum = ARGV.resolved_formulae.each
+    end
+
+    loop do
+      f = enum.next
       # Cannot test uninstalled formulae
       unless f.installed?
         ofail "Testing requires the latest version of #{f.full_name}"
