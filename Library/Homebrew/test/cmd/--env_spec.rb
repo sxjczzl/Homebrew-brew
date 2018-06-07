a3500de22be69c39ed2eb2b3dd13f1fb41aa4a17
Izaak Beekman <zbeekman@gmail.com>
2018-06-07 15:29:31 -0400

    `CMAKE_PREFIX_PATH` no longer expected in ENV spec
     - Fix tests so that they no longer expect to see `CMAKE_PREFIX_PATH`

--- Library/Homebrew/test/cmd/--env_spec.rb
+++ Library/Homebrew/test/cmd/--env_spec.rb
@@ -1,16 +1,14 @@
 describe "brew --env", :integration_test do
   it "prints the Homebrew build environment variables" do
     expect { brew "--env" }
-      .to output(/CMAKE_PREFIX_PATH="#{Regexp.escape(HOMEBREW_PREFIX)}[:"]/).to_stdout
-      .and not_to_output.to_stderr
+    not_to_output.to_stderr
       .and be_a_success
   end
 
   describe "--shell=bash" do
     it "prints the Homebrew build environment variables in Bash syntax" do
       expect { brew "--env", "--shell=bash" }
-        .to output(/export CMAKE_PREFIX_PATH="#{Regexp.quote(HOMEBREW_PREFIX)}"/).to_stdout
-        .and not_to_output.to_stderr
+      not_to_output.to_stderr
         .and be_a_success
     end
   end
@@ -18,8 +16,7 @@ describe "brew --env", :integration_test do
   describe "--shell=fish" do
     it "prints the Homebrew build environment variables in Fish syntax" do
       expect { brew "--env", "--shell=fish" }
-        .to output(/set [-]gx CMAKE_PREFIX_PATH "#{Regexp.quote(HOMEBREW_PREFIX)}"/).to_stdout
-        .and not_to_output.to_stderr
+      not_to_output.to_stderr
         .and be_a_success
     end
   end
@@ -27,8 +24,7 @@ describe "brew --env", :integration_test do
   describe "--shell=tcsh" do
     it "prints the Homebrew build environment variables in Tcsh syntax" do
       expect { brew "--env", "--shell=tcsh" }
-        .to output(/setenv CMAKE_PREFIX_PATH #{Regexp.quote(HOMEBREW_PREFIX)};/).to_stdout
-        .and not_to_output.to_stderr
+      not_to_output.to_stderr
         .and be_a_success
     end
   end
@@ -36,8 +32,7 @@ describe "brew --env", :integration_test do
   describe "--plain" do
     it "prints the Homebrew build environment variables without quotes" do
       expect { brew "--env", "--plain" }
-        .to output(/CMAKE_PREFIX_PATH: #{Regexp.quote(HOMEBREW_PREFIX)}/).to_stdout
-        .and not_to_output.to_stderr
+      not_to_output.to_stderr
         .and be_a_success
     end
   end
