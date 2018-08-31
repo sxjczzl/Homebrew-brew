927e0d2f0714551f3fa92587c8885ae845663477
Matt Coster <matthew@thecosters.co.uk>
2018-08-31 16:23:50 +0100

    Break arguments onto two lines

--- Library/Homebrew/unpack_strategy/dmg.rb
+++ Library/Homebrew/unpack_strategy/dmg.rb
@@ -137,7 +137,8 @@ module UnpackStrategy
           cdr_path = mount_dir/path.basename.sub_ext(".cdr")
 
           system_command! "hdiutil",
-                          args: ["convert", "-quiet", "-format", "UDTO", "-srcimagekey", "diskimage-class=CRawDiskImage", "-o", cdr_path, path],
+                          args: ["convert", "-quiet", "-format", "UDTO", "-srcimagekey", "diskimage-class=CRawDiskImage",
+                                 "-o", cdr_path, path],
                           verbose: verbose
 
           with_eula = system_command! "hdiutil",
