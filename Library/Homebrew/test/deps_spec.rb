3932b590c3652a9217ad9c0cfb8910db43dac358
alexbostock <alex@alexbostock.co.uk>
2018-09-19 08:47:46 +0100

    Add test for brew deps

--- Library/Homebrew/test/deps_spec.rb
+++ Library/Homebrew/test/deps_spec.rb
@@ -21,4 +21,40 @@ describe "brew deps", :integration_test do
       .and output("bar\nfoo\n").to_stdout
       .and not_to_output.to_stderr
   end
+
+  it "outputs dependencies for all formulae with the --all flag" do
+    expect { brew "deps", "--all" }
+      .to be_a_success
+      .and output(/bar: foo\nbaz: bar/).to_stdout
+      .and not_to_output.to_stderr
+  end
+
+  it "outputs a tree of dependencies with the --tree flag" do
+    expect { brew "deps", "foo", "--tree" }
+      .to be_a_success
+      .and output(/foo\s*/).to_stdout
+      .and not_to_output.to_stderr
+
+    expect { brew "deps", "baz", "--tree" }
+      .to be_a_success
+      .and output("baz\n└── bar\n    └── foo\n\n").to_stdout
+      .and not_to_output.to_stderr
+  end
+
+  it "outputs dependencies for a given list of formulae with the --for-each flag" do
+    expect { brew "deps", "foo", "bar", "--for-each" }
+      .to be_a_success
+      .and output(/bar: foo/).to_stdout
+      .and not_to_output.to_stderr
+  end
+
+  it "gives an error if run with incorrrect arguments" do
+    expect { brew "deps" }
+      .to output(/Invalid usage/).to_stderr
+      .and not_to_output.to_stdout
+
+    expect {brew "deps", "--tree" }
+      .to output(/Invalid usage/).to_stderr
+      .and not_to_output.to_stdout
+  end
 end
