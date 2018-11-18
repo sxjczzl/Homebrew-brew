c926f4e1d10f4c40cb1d454d8cec261904a5e1f2
EricFromCanada <enk3@outlook.com>
2018-11-17 19:21:34 -0500

    unpack_strategy: handle xar-compressed .pkg files

--- Library/Homebrew/unpack_strategy/pkg.rb
+++ Library/Homebrew/unpack_strategy/pkg.rb
@@ -12,5 +12,17 @@ module UnpackStrategy
       path.extname.match?(/\A.m?pkg\Z/) &&
         (path.directory? || path.magic_number.match?(/\Axar!/n))
     end
+
+    private
+
+    def extract_to_dir(unpack_dir, basename:, verbose:)
+      if path.directory?
+        super
+      else
+        system_command! "xar",
+                        args:    ["-x", "-f", path, "-C", unpack_dir],
+                        verbose: verbose
+      end
+    end
   end
 end
