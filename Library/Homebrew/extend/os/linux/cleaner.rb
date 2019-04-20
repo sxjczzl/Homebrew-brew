89206590e70a4ca2d107df929d3f39072ead64b5
Chris J. Breisch <chris.breisch@imglobal.com>
2019-04-19 20:00:30 -0400

    forgot to update linux version of cleaner

--- Library/Homebrew/extend/os/linux/cleaner.rb
+++ Library/Homebrew/extend/os/linux/cleaner.rb
@@ -2,10 +2,6 @@ class Cleaner
   private
 
   def executable_path?(path)
-    if OS.cygwin?
-       path.extname == ".exe" || path.text_executable?
-    else
-       path.elf? || path.text_executable?
-    end
+    path.extname == ".exe" || path.text_executable?
   end
 end
