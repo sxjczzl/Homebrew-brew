3654687a8e66081ffb9b2ec616ceba71f3509dac
Chris J. Breisch <chris.breisch@imglobal.com>
2019-04-26 21:39:15 -0400

    more fixes for os-cygwin

--- Library/Homebrew/extend/os/software_spec.rb
+++ Library/Homebrew/extend/os/software_spec.rb
@@ -1,5 +1,5 @@
 if OS.linux?
-  require "extend/os/linux/software_spec" 
+  require "extend/os/linux/software_spec"
 elsif OS.cygwin?
-  require "extend/os/cygwin/software_spec" 
+  require "extend/os/cygwin/software_spec"
 end
