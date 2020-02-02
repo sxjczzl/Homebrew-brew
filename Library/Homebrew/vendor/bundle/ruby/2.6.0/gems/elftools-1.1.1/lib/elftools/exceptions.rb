2eb367832163307159268aefbf79c952efef1e1a
William Woodruff <william@trailofbits.com>
2020-02-02 11:30:18 +0100

    [WIP] vendor: Bring in patchelf.rb

---
+++ Library/Homebrew/vendor/bundle/ruby/2.6.0/gems/elftools-1.1.1/lib/elftools/exceptions.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module ELFTools
+  # Being raised when parsing error.
+  class ELFError < StandardError; end
+end
---
+++ Library/Homebrew/vendor/bundle/ruby/2.6.0/gems/patchelf-0.1.0/lib/patchelf/version.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module PatchELF
+  # Current gem version.
+  VERSION = '0.1.0'.freeze
+end
