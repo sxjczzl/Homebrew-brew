dedfc284628ae0cf38e9d0ff83dd16c14611c188
Seeker <meaningseeking@protonmail.com>
2020-02-25 08:24:07 -0800

    test/cask/conflicts_with_spec: attempt at fixing test

--- Library/Homebrew/test/cask/conflicts_with_spec.rb
+++ Library/Homebrew/test/cask/conflicts_with_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "test/support/fixtures/testball"
+
 describe "conflicts_with", :cask do
   describe "conflicts_with cask" do
     let(:local_caffeine) {
@@ -26,24 +28,26 @@ end
 
 describe "conflicts_with", :formula do
   describe "conflicts_with formula" do
-    let(:cmake_formula) {
-      Formula["cmake"]
+    let(:testball_formula) {
+      # FIXME: figure out how to add formula as Formula["testball"]
+      # so that conflict resolution can work properly
+      Testball.new
     }
 
-    let(:cmake_cask) {
-      Cask::CaskLoader.load(cask_path("cmake"))
+    let(:with_conflicts_with_formula_cask) {
+      Cask::CaskLoader.load(cask_path("with-conflicts-with-formula"))
     }
 
     it "installs a Formula and a conflicting Cask" do
-      FormulaInstaller.new(cmake_formula).install
+      FormulaInstaller.new(testball_formula).install
 
-      expect(cmake_formula).to be_latest_version_installed
+      expect(testball_formula).to be_latest_version_installed
 
       expect {
-        Cask::Installer.new(cmake_cask).install
-      }.to raise_error(Cask::CaskConflictError, "Cask 'cmake' conflicts with 'cmake'.")
+        Cask::Installer.new(with_conflicts_with_formula_cask).install
+      }.to raise_error(Cask::CaskFormulaConflictError, "Cask 'with-conflicts-with-formula' conflicts with formula 'testball'.")
 
-      expect(cmake_cask).not_to be_installed
+      expect(with_conflicts_with_formula_cask).not_to be_installed
     end
   end
 end
