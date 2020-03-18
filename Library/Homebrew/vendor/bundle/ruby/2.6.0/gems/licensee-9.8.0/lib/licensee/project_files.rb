c079bdf159c528a4c86a2eae006efc5914943f11
Michelle Nguyen <michelle.b.nguy@gmail.com>
2020-03-18 12:19:19 -0400

    Revert "brew vendor-gems: commit updates."
    This reverts commit 30e9678cb2ae66a69db514c469b8b5a8c2805cb6.

--- Library/Homebrew/vendor/bundle/ruby/2.6.0/gems/licensee-9.8.0/lib/licensee/project_files.rb
+++
@@ -1,8 +0,0 @@
-module Licensee
-  module ProjectFiles
-    autoload :ProjectFile, 'licensee/project_files/project_file'
-    autoload :LicenseFile, 'licensee/project_files/license_file'
-    autoload :PackageManagerFile, 'licensee/project_files/package_manager_file'
-    autoload :ReadmeFile, 'licensee/project_files/readme_file'
-  end
-end
--- Library/Homebrew/vendor/bundle/ruby/2.6.0/gems/licensee-9.8.0/lib/licensee/version.rb
+++
@@ -1,3 +0,0 @@
-module Licensee
-  VERSION = '9.8.0'.freeze
-end
