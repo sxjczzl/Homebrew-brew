97cb2706ed427d7d44231deaebb78fb975cc01b8
Kid <44045911+kidonng@users.noreply.github.com>
2021-03-09 08:27:22 +0800

    Allow overriding environment variables defined with write_env_script

--- Library/Homebrew/extend/pathname.rb
+++ Library/Homebrew/extend/pathname.rb
@@ -392,7 +392,7 @@ class Pathname
       args = nil
     end
     env_export = +""
-    env.each { |key, value| env_export << "#{key}=\"#{value}\" " }
+    env.each { |key, value| env_export << "#{key}=${#{key}-'#{value}'} " }
     dirname.mkpath
     write <<~SH
       #!/bin/bash
