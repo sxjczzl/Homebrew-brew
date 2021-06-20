ad0691ad27558f6790b63953bc7412b8a804ec5f
Radek Simko <radek.simko@gmail.com>
2021-06-20 22:39:54 +0100

    Read multiple depends_on :arch as 'any of'

--- Library/Homebrew/requirements/arch_requirement.rb
+++ Library/Homebrew/requirements/arch_requirement.rb
@@ -14,21 +14,37 @@ class ArchRequirement < Requirement
   attr_reader :arch
 
   def initialize(tags)
-    @arch = tags.shift
+    @arch = tags
     super(tags)
   end
 
   satisfy(build_env: false) do
-    case @arch
+    if @arch.is_a? Array
+      next true if @arch.empty?
+
+      satisfied = T.let(false, T::Boolean)
+      @arch.each do |arch|
+        satisfied = satisfies_arch(arch)
+        break if satisfied
+      end
+
+      next satisfied
+    end
+    satisfies_arch(@arch)
+  end
+
+  sig { params(arch: Symbol).returns(T::Boolean) }
+  def satisfies_arch(arch)
+    case arch
     when :x86_64 then Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
     when :arm64 then Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    when :arm, :intel, :ppc then Hardware::CPU.type == @arch
+    when :arm, :intel, :ppc then Hardware::CPU.type == arch
     end
   end
 
   sig { returns(String) }
   def message
-    "The #{@arch} architecture is required for this software."
+    "One of #{@arch} architectures is required for this software."
   end
 
   def inspect
--- Library/Homebrew/test/requirements/arch_requirement_spec.rb
+++ Library/Homebrew/test/requirements/arch_requirement_spec.rb
@@ -4,11 +4,33 @@
 require "requirements/arch_requirement"
 
 describe ArchRequirement do
-  subject(:requirement) { described_class.new([Hardware::CPU.type]) }
+  subject(:requirement) {
+    described_class.new(arch)
+  }
 
   describe "#satisfied?" do
-    it "supports architecture symbols" do
-      expect(requirement).to be_satisfied
+    context "when given current architecture" do
+      let(:arch) { [Hardware::CPU.type] }
+
+      it "satisfies requirement" do
+        expect(requirement).to be_satisfied
+      end
+    end
+
+    context "when given all architectures" do
+      let(:arch) { [:x86_64, :arm64, :arm, :intel, :ppc] }
+
+      it "satisfies requirement" do
+        expect(requirement).to be_satisfied
+      end
+    end
+
+    context "when given empty array" do
+      let(:arch) { [] }
+
+      it "satisfies requirement" do
+        expect(requirement).to be_satisfied
+      end
     end
   end
 end
