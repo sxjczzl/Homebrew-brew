ec28a00d423a58784ea0e36ecf423ff34f364f07
Rylan Polster <rslpolster@gmail.com>
2022-01-20 20:54:12 -0500

    Fix `brew services` test

--- Library/Homebrew/test/cmd/services_spec.rb
+++ Library/Homebrew/test/cmd/services_spec.rb
@@ -6,8 +6,8 @@ describe "brew services", :integration_test, :needs_macos, :needs_network do
     setup_remote_tap "homebrew/services"
 
     expect { brew "services", "list" }
-      .to output("Warning: No services available to control with `brew services`\n").to_stderr
+      .to be_a_success
+      .and not_to_output.to_stderr
       .and not_to_output.to_stdout
-      .and be_a_success
   end
 end
