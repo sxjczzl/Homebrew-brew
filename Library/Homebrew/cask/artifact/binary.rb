0fea972ae3f73720a63f8421392faca0041532f9
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2022-08-04 09:51:31 -0700

    Add shimscript arg to cask DSL

--- Library/Homebrew/cask/artifact/binary.rb
+++ Library/Homebrew/cask/artifact/binary.rb
@@ -3,13 +3,46 @@
 
 require "cask/artifact/symlinked"
 
+require "extend/hash_validator"
+using HashValidator
+
 module Cask
   module Artifact
     # Artifact corresponding to the `binary` stanza.
     #
     # @api private
     class Binary < Symlinked
+      def self.from_args(cask, *args)
+        source, args = args
+
+        if args
+          raise CaskInvalidError unless args.respond_to?(:keys)
+
+          args.assert_valid_keys!(:target, :shimscript)
+        end
+
+        args ||= {}
+
+        new(cask, source, **args)
+      end
+
+      def initialize(cask, source, target: nil, shimscript: nil)
+        super(cask, source, target: target)
+        return if shimscript.nil?
+
+        @shimscript_source = @source
+        @source_string = shimscript.to_s
+        @source = cask.staged_path.join(shimscript)
+      end
+
       def link(command: nil, **options)
+        if @shimscript_source
+          File.write source, <<~EOS
+            #!/bin/bash
+            exec '#{@shimscript_source}' "$@"
+          EOS
+        end
+
         super(command: command, **options)
         return if source.executable?
 
